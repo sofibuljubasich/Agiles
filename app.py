34ccc1ec325a3b12b0a745cd491662f44a56b929
Sofia Buljubasich <sofiabuljuba@gmail.com>
2023-11-05 22:20:51 -0300

    add UI

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, session,render_template, request, redirect, url_for
+from main import Ahorcado
+
+app = Flask(__name__)
+app.secret_key = 'GiveUsFullMarksPleaseeeeeeee' 
+
+
+juego = Ahorcado()
+
+def iniciar_juego():
+    juego.iniciar()
+
+    session['palabra']= juego.palabraAdivinar
+    session['palabraAhorcado'] = juego.palabraAhocardo
+    session['vidas'] = juego.vidas
+    session['incorrectas']=juego.letras_incorrectas
+
+@app.route('/')
+def index(): 
+ # If the game hasn't started yet, initialize it
+    if 'palabra' not in session:
+        iniciar_juego()
+    
+    return render_template('jugar.html',palabra=session['palabra'],palabraAhorcado=session['palabraAhorcado'],vidas=session['vidas'],incorrectas=session['incorrectas'])
+
+#def home():
+ #   return render_template('home.html')
+
+@app.route('/nuevo_juego')
+def nuevo_juego():
+    juego.iniciar()
+
+    session['palabra']= juego.palabraAdivinar
+    session['palabraAhorcado'] = juego.palabraAhocardo
+    session['vidas'] = juego.vidas
+    session['incorrectas']=juego.letras_incorrectas
+    return render_template('jugar.html',palabra=session['palabra'],palabraAhorcado=session['palabraAhorcado'],vidas=session['vidas'],incorrectas=session['incorrectas'])
+
+
+@app.route('/adivinar', methods=['POST'])
+def adivinar():
+
+    input = request.form['input']
+
+    if juego.arriesgar(input):
+        if juego.gano():
+            return render_template("win.html", palabra=session['palabra'])
+        
+        session['palabraAhorcado'] = juego.palabraAhocardo
+        return redirect(url_for("index"))
+
+    if juego.perdio():
+        return render_template("lose.html", palabra=session['palabra'])
+
+    
+    session['palabra']= juego.palabraAdivinar
+    session['palabraAhorcado'] = juego.palabraAhocardo
+    session['vidas'] = juego.vidas
+    session['incorrectas']=juego.letras_incorrectas
+
+    return redirect(url_for("index"))
+
+
+    
+
+
+
+@app.route("/play_again", methods=["POST"])
+def play_again():
+    iniciar_juego()
+    return redirect(url_for("index"))
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,28 +1,52 @@
+import random
+LP = ["python", "flask", "hangman", "web", "programming"]
+
 class Ahorcado:
     def __init__(self):
-        self.palabraAdivinar = "hola"
+      
         self.vidas = 6
         self.letras_incorrectas = []
-        self.palabraAhocardo = ["_","_","_","_"]
+        self.palabraAdivinar = "" 
+        self.palabraAhocardo = []
         
+    def iniciar(self):
+        self.palabraAdivinar = random.choice(LP)
+        self.palabraAhocardo = ["_" for _ in self.palabraAdivinar]
+        self.vidas = 6
+        self.letras_incorrectas = []
 
     def get_palabra(self):
         return self.palabraAdivinar
         
     def arriesgar_palabra(self,palabra):
+        
+        
         return palabra == self.palabraAdivinar
     
+   
     def arriesgar_letra(self,letra):
         if not self.chequear_letra_repetida(letra):
             if (letra in self.palabraAdivinar):
-                posicion = self.devolver_posicion_letra(letra)
-                self.palabraAhocardo[posicion]=letra
-                print(self.palabraAhocardo)
+                self.reemplazar_letra(letra)
                 return True
             else:
                 self.descontar_vida()
                 self.letras_incorrectas.append(letra)
                 return False
+    
+    def reemplazar_letra(self,input):
+        for idx, letra in enumerate(self.palabraAdivinar):
+            if input == letra:
+                self.palabraAhocardo[idx]=input
+          
+    def arriesgar(self,input):
+        print("ingresa",input)
+        if len(input) == 1:
+            print("letra")
+            return self.arriesgar_letra(input)
+        else:
+            print("palarb")
+            return self.arriesgar_palabra(input)
     
     def chequear_letra_repetida(self,letra):
         if ((letra in self.letras_incorrectas) or (letra in self.palabraAhocardo)):
@@ -38,4 +62,15 @@ class Ahorcado:
 
     def mostrar_letras_incorrectas(self):
         return self.letras_incorrectas
+    
+    def mostrar_palabra():
+        pass
+
+    def gano(self):
+        estado = ''.join(self.palabraAhocardo)
+        print(estado)
+        return estado == self.palabraAdivinar
+    
+    def perdio(self):
+        return (self.vidas <1)
     
